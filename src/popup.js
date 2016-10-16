0d18afb12fde5460cea1da3d2ad84656ef400d27
alexsirr <alexsirruw@gmail.com>
2016-10-16 05:52:16 -0700

    popup styling

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,27 @@
 var bg = chrome.extension.getBackgroundPage()
 
-
-$("#toggle").on("click", function(e) {
+function toggle() {
     var h = $("#status")
     if (bg.isEnabled) {
-        h.text("Currently Disabled")
+        h.text("Currently Disabled").css("color", "red")
         bg.isEnabled = false
     } else {
-        h.text("Currently Enabled")
+        h.text("Currently Enabled").css("color", "green")
         bg.isEnabled = true
     }
+}
+
+function checkStatus() {
+    var h = $("#status")
+    if (!bg.isEnabled) {
+        h.text("Currently Disabled").css("color", "red")
+    } else {
+        h.text("Currently Enabled").css("color", "green")
+    }
+}
+
+$("#toggle").on("click", function(e) {
+    toggle()
 })
+
+checkStatus()
