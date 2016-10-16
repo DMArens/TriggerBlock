ca0f5c5169ac26a98b6bab34224ac7ba82ea8f36
DMArens <Darens103@gmail.com>
2016-10-16 13:11:52 -0700

    made context menu

--- src/background.js
+++ src/background.js
@@ -1,6 +1,18 @@
 var isEnabled = true
 
+function blockHandler(info, tab) {
+	console.log(info);
+	console.log(tab);
+	chrome.tabs.sendMessage(tab.id, "blockTrigger", function() {});
+}
+
 chrome.extension.onMessage.addListener(function(message,sender,sendResponse){
   if(message.text == "EnabledCheck")
     sendResponse({isEnabled:isEnabled});
 });
+
+chrome.contextMenus.create({
+    "title": "Block Trigger",
+    "contexts": ["image"],
+    "onclick" : blockHandler
+  });
