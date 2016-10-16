5a033f06245932ceaedb1af4f4a10af3f130c4d9
alexsirr <alexsirruw@gmail.com>
2016-10-16 01:01:56 -0700

    more option changes

--- src/options.js
+++ src/options.js
@@ -1,23 +1,40 @@
+var triggerStore = null
 // add a trigger word to the list
 function addTrigger() {
     var newTrigger = document.getElementById("trigger-input").value 
     chrome.storage.sync.get(
-        "trigger"
+        "triggers"
     , function(data) {
         // log pre added data
         console.log(data)
         var obj = {}
-        list = data.trigger
+        list = data.triggers
         list.push(newTrigger)
-        obj["trigger"] = list 
+        obj["triggers"] = list 
         chrome.storage.sync.set(obj, function(){
             // here for testing
-            chrome.storage.sync.get("trigger", function(data){console.log(data)})
+            var list = $("#current-triggers")
+            list.append("<li>" + newTrigger + "</li>")
+            chrome.storage.sync.get("triggers", function(data){console.log(data)})
         })
     });
 }
 
-// reset storage each time for now
-chrome.storage.sync.set({"trigger": []}, function(){})
+function listTriggers() {
+    var list = $("#current-triggers")
+    for (var item in triggerStore.triggers) {
+        list.append("<li>" + triggerStore.triggers[item] + "<button class='remove'>blah</button></li>")
+    }
+}
+
+chrome.storage.sync.get("triggers", function(data){triggerStore = data})
  
 document.getElementById('save-trigger').addEventListener('click', addTrigger);
+
+$(document).ready(function() {
+    listTriggers()
+    $(".remove").click(function(e){
+        console.log("clicked")
+        
+    })
+})
