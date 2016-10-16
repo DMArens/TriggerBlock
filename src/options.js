380853371b2ec7b46731c7ecb86a61cfae4ca901
alexsirr <alexsirruw@gmail.com>
2016-10-16 00:18:28 -0700

    fix options storage

--- src/options.js
+++ src/options.js
@@ -1,10 +1,23 @@
-function save_options() {
-    obj = {test: "test"}
-  chrome.storage.sync.set({
-    obj
-  }, function() {
-    console.log("saved to sync")
-  });
+// add a trigger word to the list
+function addTrigger() {
+    var newTrigger = document.getElementById("trigger-input").value 
+    chrome.storage.sync.get(
+        "trigger"
+    , function(data) {
+        // log pre added data
+        console.log(data)
+        var obj = {}
+        list = data.trigger
+        list.push(newTrigger)
+        obj["trigger"] = list 
+        chrome.storage.sync.set(obj, function(){
+            // here for testing
+            chrome.storage.sync.get("trigger", function(data){console.log(data)})
+        })
+    });
 }
 
-document.getElementById('save-trigger').addEventListener('click', save_options);
+// reset storage each time for now
+chrome.storage.sync.set({"trigger": []}, function(){})
+ 
+document.getElementById('save-trigger').addEventListener('click', addTrigger);
